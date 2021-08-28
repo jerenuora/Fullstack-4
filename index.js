cf6facee1533bc3c4207cb5f16298aa419a31707
Jere Nuora <jere.nuora@gmail.com>
2021-08-28 13:27:21 +0300

    Quite a few modules added at once

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
+const app = require('./app')
+
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const { errorMonitor } = require('events')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => logger.error(error))
@@ -23,23 +14,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
