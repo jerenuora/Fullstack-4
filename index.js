2651a388d173844ff243528a9742d6fe5301084e
Jere Nuora <jere.nuora@gmail.com>
2021-08-28 13:30:07 +0300

    Index.js cleaned up

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => logger.error(error))
-
-app.use(cors())
-app.use(express.json())
-
+const server = http.createServer(app)
 
 const PORT = config.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
