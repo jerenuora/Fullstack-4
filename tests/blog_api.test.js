82c4b57cb77a877c256f48b85904b97cb4b407aa
Jere Nuora <jere.nuora@gmail.com>
2022-01-05 16:52:58 +0200

    tests 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -24,6 +24,22 @@ test('blog identifier field is id', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
+test('a blog can be posted', async () => {
+    const newBlog = {
+        title: 'A new post',
+        author: 'Jere'
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAfterPost = await api.get('/api/blogs')
+    expect(blogsAfterPost.body).toHaveLength(helper.initialBlogs.length + 1)
+    const blogTitles = blogsAfterPost.body.map(b => b.title)
+    expect(blogTitles).toContain('A new post')
+})
 
 
 afterAll(() => {
