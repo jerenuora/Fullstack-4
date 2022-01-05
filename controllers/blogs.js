00d6187fffbec365917d91ef472220a5049e3dc5
Jere Nuora <jere.nuora@gmail.com>
2022-01-05 18:05:55 +0200

    4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -23,6 +23,15 @@ blogsRouter.delete('/:id', async (req,res) => {
   res.status(204).end()
 })
 
+blogsRouter.put('/:id', async (req,res) => {
+  const body = req.body
 
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id,blog)
+  res.json(updatedBlog.toJSON())
+})
 
 module.exports = blogsRouter
