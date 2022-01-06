553cda7ac184d8b44b925aefe4344989f9e49de0
Jere Nuora <jere.nuora@gmail.com>
2022-01-06 18:12:03 +0200

    4.16 other requirements

--- controllers/users.js
+++ controllers/users.js
@@ -2,27 +2,30 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
-  const body = request.body
+usersRouter.post('/', async (req, res) => {
+    const body = req.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const saltRounds = 10
+    if (body.password.length < 3) {
+        return res.status(400).json({ error: 'invalid password' })
+    }
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.json(savedUser)
+    res.json(savedUser)
 })
 
 usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
     response.json(users.map(u => u.toJSON()))
-  })
-  
+})
+
 
 module.exports = usersRouter
