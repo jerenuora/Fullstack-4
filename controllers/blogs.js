23ddeeefdfb1fcaeedafc59ab54c9eaeb0b4945f
Jere Nuora <jere.nuora@gmail.com>
2022-01-06 19:06:27 +0200

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+  .find({}).populate('user', {username: 1, name: 1})
   res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  const body = req.body
+  
+  const user = await User.findById(body.userId)
+  console.log(body)
+  const blog = new Blog({
+    url:body.url,
+    title:body.title,
+    author:body.author,
+    user:user._id
+  })
 
   if (blog.title === undefined) {
     res.status(400).json('Bad request')
@@ -15,6 +26,9 @@ blogsRouter.post('/', async (req, res) => {
     res.status(400).json('Bad request ')
   }  else {
     const result  = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+  
     res.status(201).json(result)}
 })
 
--- controllers/users.js
+++ controllers/users.js
@@ -23,7 +23,8 @@ usersRouter.post('/', async (req, res) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('blogs', {url: 1, title: 1, author: 1})
     response.json(users.map(u => u.toJSON()))
 })
 
