4808ecdd605a60aec398ed91877418723d6f6047
Jere Nuora <jere.nuora@gmail.com>
2022-01-06 17:53:46 +0200

    4.16 part unique usr

--- models/user.js
+++ models/user.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 
 const userSchema = mongoose.Schema({
-    username: String,
-    name: String,
-    passwordHash: String,
-    blogs: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Blog'
-      }
-    ],
-  })
-  
-  userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-      // the passwordHash should not be revealed
-      delete returnedObject.passwordHash
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
     }
-  })
-  
-  const User = mongoose.model('User', userSchema)
-  
-  module.exports = User
+  ],
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
